2c328870b05d7b17a392cbfebd9682d09d1a77c8
gerardjr42 <ggarciajr12@gmail.com>
2024-03-30 16:47:15 -0400

    add variable and functions for useState

---
+++ src/components/SearchParams.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
+
+
+}
+
+export default SearchParams;
